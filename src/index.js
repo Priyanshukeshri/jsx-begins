1110d1105e2dc0e280eb32ee734b2f2804819042
priyanshu kumar keshri <priyanshukumarkeshri6@gmail.com>
2022-04-07 14:22:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+    <div>
+      <h1>Hello world</h1>
+      <p>this is a paragraph</p>
+    </div>,
+    document.getElementById("root")
+);
